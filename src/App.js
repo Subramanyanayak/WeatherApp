1841d9d1002bf8049a8c6300c3ad69265df4e1f2
Subramanyanayak <kssubramanyanayak1997@gmail.com>
2021-05-21 11:25:05 +0530

    Api link changed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   useEffect(() =>{
     setTimeout(() => {
-      fetch('http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40')
+      fetch('https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40')
       .then(response => response.json())
       .then(response => setPost(response.list)) ;
     },900)
